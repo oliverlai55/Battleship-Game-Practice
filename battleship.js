792b2dc9dc6aaa80bccca360607fff746c0cbdee
oliverlai55 <oliverlai55@gmail.com>
2015-11-17 12:03:36 -0500

    js changes

--- battleship.js
+++ battleship.js
@@ -7,19 +7,25 @@ var guesses = 0;
 var isSunk = false;
 var stats = "You took" + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
 
-while (isSunk == flase){
-	guess = prompt("Ready, aim fire! (enter a number 0-6):");
-	if(guess < 0 || guess > 6){
-		alert("Please enter a valid cell number!");
-	} else{
-		guesses = guess + 1;
-		if (guess == location1 || guess == location2 || guess == location3){
-			hits = hits + 1;
-			if (hits == 3){
-				isSunk = true;
-				alert("You sank my battleship!");
-				alert(stats);
+function game(){
+	while (isSunk == false){
+		guess = prompt("Ready, aim fire! (enter a number 0-6):");
+		if(guess < 0 || guess > 6){
+			alert("Please enter a valid cell number!");
+		} else{
+			guesses = guess + 1;
+			if (guess == location1 || guess == location2 || guess == location3){
+				alert("HIT!");
+				hits = hits + 1;
+				if (hits == 3){
+					isSunk = true;
+					alert("You sank my battleship!");
+					alert(stats);
+				}
+			}else{
+				alert("MISS");
 			}
 		}
 	}
 }
+game();
